1f6861bb5f2518888580803c4ef00a26ac5124fe
Ankush Mukherjee <ankush.mukherjee24@gmail.com>
2023-01-26 11:55:42 +0530

    server.js file- starting point of the server

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const app = require("./app");
-
-
 const dotenv = require("dotenv");
+const connectDatabase = require("./database/database");
 
 
 // config path
 dotenv.config({path:"config/config.env"})
 
+// connection with database
+connectDatabase();
 
 app.listen(process.env.PORT, ()=>{
     console.log(`server is running successfully`)
