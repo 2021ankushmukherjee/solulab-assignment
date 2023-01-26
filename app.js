9d35da9f6ff6db1d60cc7b9352dff1241f4cee5c
Ankush Mukherjee <ankush.mukherjee24@gmail.com>
2023-01-26 11:55:05 +0530

    app.js file

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 
+// bodyParser
+app.use(express.json())
+// route imports
+const category = require("./routes/categoryRoute")
+const product = require("./routes/productRoute");
+
+app.use("/category", category)
+app.use("/product", product);
 
 
 module.exports = app;
