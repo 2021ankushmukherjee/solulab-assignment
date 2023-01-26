4467917f047b673713a7f57225329e3ddd8e60cc
Ankush Mukherjee <ankush.mukherjee24@gmail.com>
2023-01-26 11:50:18 +0530

    product controller

---
+++ routes/productRoute.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { getAllProducts, createProduct, updateProduct, deleteProduct, getProduct } = require("../controllers/productController");
+const router = express.Router();
+
+//router.route("/products").get(getAllProducts);
+router.route("/create").post(createProduct);
+router.route("/readall").get(getAllProducts);
+router.route("/read").get(getProduct);
+router.route("/update/:id").put(updateProduct);
+router.route("/delete/:id").delete(deleteProduct);
+
+module.exports=router;
+
